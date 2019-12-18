78331ca24ffb5cd44b6135d47a5920cf1b499ddb
haigen.zhu <haigen.zhu@tcl.com>
2019-12-18 17:40:54 +0800

    init

---
+++ app/src/main/java/com/mike/animation/home/MainActivity.java
@@ -0,0 +1,43 @@
+package com.mike.animation.home;
+
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+
+import com.mike.animation.R;
+import com.mike.animation.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+
+public class MainActivity extends BaseActivity {
+
+    @BindView(R.id.recyclerview)
+    RecyclerView recyclerView;
+
+    List<ItemDataBean> mDataList = new ArrayList<>();
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_main;
+    }
+
+    @Override
+    public void afterSetContentView() {
+        initDataList();
+        LinearLayoutManager llm = new LinearLayoutManager(this, RecyclerView.VERTICAL, false);
+        recyclerView.setLayoutManager(llm);
+        HomeListAdapter homeListAdapter = new HomeListAdapter(this, mDataList);
+        recyclerView.setAdapter(homeListAdapter);
+    }
+
+    private void initDataList() {
+        mDataList.add(new ItemDataBean("aniamtion",
+                com.mike.animation.test.MainActivity.class, ColorGenerator.getInstance().getColor()));
+
+    }
+}
---
+++ app/src/main/java/com/mike/animation/test/MainActivity.java
@@ -0,0 +1,22 @@
+package com.mike.animation.test;
+
+
+import com.mike.animation.R;
+import com.mike.animation.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    private static final String TAG = "MainActivity";
+
+    @Override
+    public int getContentViewId() {
+        return R.layout.activity_test;
+    }
+
+    @Override
+    public void afterSetContentView() {
+
+    }
+
+
+}
